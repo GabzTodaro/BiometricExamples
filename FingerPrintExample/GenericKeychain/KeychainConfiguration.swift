d95a55e3c4e568728a858aba52c0a51097bc2cb9
Gabriel Todaro <gabriel.todaro@5by5.com.br>
2018-10-04 17:51:30 -0300

    first commit

---
+++ FingerPrintExample/GenericKeychain/KeychainConfiguration.swift
@@ -0,0 +1,9 @@
+//
+//  KeychainConfiguration.swift
+//  FingerPrintExample
+//
+//  Created by Gabriel Todaro on 04/10/18.
+//  Copyright © 2018 Todaro. All rights reserved.
+//
+
+import Foundation
